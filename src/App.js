fe8def99d3bd3e650328a2073487ffaceda9359b
Andres Garcia <andres.gar@gmail.com>
2022-05-25 12:46:02 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(result => {
+        setPosts(result)
+      })
+    
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(result => {
+        setUsers(result)
+      })
+  }, [])
+
+  const getUserName = userId => {
+    const user = users.find(({ id }) => id === userId)
+
+    if (user) {
+      return user.name
+    }
+
+    return ''
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+        padding: '3rem'
+      }}>
+        { posts.map(({
+          id,
+          userId,
+          title,
+          body,
+        }) => (
+          <div key={id} style={{
+            marginBottom: '1rem'
+          }}>
+            <span style={{display: 'block'}}>{ title }</span>
+            <span style={{display: 'block'}}>{ body }</span>
+            <span style={{display: 'block'}}>{ getUserName(userId) }</span>
+          </div>
+        )) }
+      </div>
     </div>
   );
 }
